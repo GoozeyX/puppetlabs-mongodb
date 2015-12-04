353889254982c9e5f478e9d124511d64e6078284
Alexander Brovman <alexander.brovman@s-itsolutions.at>
2015-12-04 14:04:24 +0100

    fixing yum.pp indenting

--- manifests/repo/yum.pp
+++ manifests/repo/yum.pp
@@ -5,11 +5,11 @@ class mongodb::repo::yum inherits mongodb::repo {
 
   if($::mongodb::repo::ensure == 'present' or $::mongodb::repo::ensure == true) {
     yumrepo { 'mongodb':
-      descr    => $::mongodb::repo::description,
-      baseurl  => $::mongodb::repo::location,
-      gpgcheck => '0',
-      enabled  => '1',
-      proxy    => $::mongodb::repo::proxy,
+      descr          => $::mongodb::repo::description,
+      baseurl        => $::mongodb::repo::location,
+      gpgcheck       => '0',
+      enabled        => '1',
+      proxy          => $::mongodb::repo::proxy,
       proxy_username => $::mongodb::repo::proxy_username,
       proxy_password => $::mongodb::repo::proxy_password,
     }
